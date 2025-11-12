a63cef3e059a5d6966b1f0107e579993958b8fda
Jorge Martinez <tnlcomputer@gmail.com>
2025-11-12 14:59:12 -0300

    'avanzamos en el curso y vemos constructor, modifier y payable, como el require para validar que es solo el propietarios que usa la funcion.'

--- contracts/example.sol
+++ contracts/example.sol
@@ -6,10 +6,30 @@ pragma solidity ^0.8.12;
 
 contract Example {
     uint number;
+    address owner;
 
-    function setNumber(uint x) public {
+    // como declaramos un constructor
+    constructor () {
+        owner = msg.sender;
+    }
+
+    // Se utiliza para crear nuestros propieos modificadores y cuando queremos que sea solo el 
+    // propietario que use cual o tal funcion solo ponemos el modificador despues de public
+    modifier  onlyOwner {
+        require(owner == msg.sender, "ERROR: Tan solo el propietario puede ejecutar esta funcion");
+        // siempre se utiliza para terminar un modifier _;
+        _;
+    }
+
+    function setNumber(uint x) public onlyOwner  {
+        require(owner == msg.sender, "ERROR: Tan solo el propietario puede ejecutar esta funcion");
         number = x;
     }
+
+    // payable se utiliza para recibir pagos para criptomoneda
+    // function invitemeaCoffee() public payable {
+    //     require(msg.value == 55555555);
+    // }
 
     function getNumber() public view returns(uint) {
         return number;
